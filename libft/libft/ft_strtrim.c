ebd85a095be8e4c45cd75863c1fc7e0536906f34
Nkipohcs <117274957+Nkipohcs@users.noreply.github.com>
2022-11-20 00:49:48 +0100

    Add files via upload

---
+++ libft/libft/ft_strtrim.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nschwob <nschwob@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 15:07:05 by nschwob           #+#    #+#             */
+/*   Updated: 2022/11/20 00:29:40 by nschwob          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//strtrim removes all whitespace chars from the beginning and the end of a str.
+
+#include "libft.h"
+
+static int	ft_is_trim_possible(char c, char const *set)
+{
+	int		count;
+
+	count = -1;
+	while (set[++count])
+		if (set[count] == c)
+			return (1);
+	return (0);
+}
+
+static int	ft_get_size(char const *s1, char const *set)
+{
+	int		count;
+	int		size;
+
+	size = ft_strlen(s1);
+	count = 0;
+	while (ft_is_trim_possible(s1[size - count - 1], set))
+		count++;
+	return (size - count);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		count;
+	int		size;
+	char	*tab;
+
+	count = 0;
+	size = 0;
+	if (!s1)
+		return (0);
+	if (!set)
+		return (ft_strdup(s1));
+	while (ft_is_trim_possible(s1[count], set))
+		count++;
+	if (count == (int)ft_strlen(s1))
+		return (ft_strdup(""));
+	size = ft_get_size(s1 + count, set) + 1;
+	if (!(tab = (char *)malloc((size) * sizeof(char))))
+		return (NULL);
+	ft_strlcpy(tab, s1 + count, size);
+	return (tab);
+}
