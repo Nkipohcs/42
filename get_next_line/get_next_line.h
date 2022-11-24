4aee2c1eddf1f3086780a9f7a8a2a106d1113630
͚̤̖͕̪̝͇̟͈̙͉̒͐̎͋͂͛̄̋́̈̇͐̂̋ͅ-̬̰͇̮̟̖̞̬̙̙̯͎͔̣̎̐͋̈́́̒̇̍̑̚H̰͍̙͍̘̘͙͔̊͐̀̀͗̊̚̚Ɔ͖̞͕̭̖̞̠̘̪̫̲̥̣̬͎͈̀̇͋͛͆̔̇̉͑̇̾͌͛̎̍̎Ś͙̯͈͕̱͉̤̭̏͋̃̉͒̑̃̏̾̿̇̃̒͋͌ͅN͉͚̰̫̞̪͈̭̤̞͔͓͎̣̄̓̅̋̈́̉͆̊-̪͈͈̭͎͍̭̥̭͈̣̫̙͍̉̂́̋͆̉̾̄͋̇͌̈̏́̈̚.͓̗̰͙̖̥̠̭̱̙̯̃̀͗͆̃̈́͂͂͐͌͐ <117274957+Nkipohcs@users.noreply.github.com>
2022-11-24 22:39:28 +0100

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -13,4 +13,18 @@
 #ifndef GET_NEXT_LINE.H
 # define GET_NEXT_LINE.H
 
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 7
+# endif BUFFER_SIZE
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strcpy(char *dest, char *src);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
 #endif
