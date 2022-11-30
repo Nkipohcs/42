624b3ed980869e9a209c0e9d412327f713156a96
͚̤̖͕̪̝͇̟͈̙͉̒͐̎͋͂͛̄̋́̈̇͐̂̋ͅ-̬̰͇̮̟̖̞̬̙̙̯͎͔̣̎̐͋̈́́̒̇̍̑̚H̰͍̙͍̘̘͙͔̊͐̀̀͗̊̚̚Ɔ͖̞͕̭̖̞̠̘̪̫̲̥̣̬͎͈̀̇͋͛͆̔̇̉͑̇̾͌͛̎̍̎Ś͙̯͈͕̱͉̤̭̏͋̃̉͒̑̃̏̾̿̇̃̒͋͌ͅN͉͚̰̫̞̪͈̭̤̞͔͓͎̣̄̓̅̋̈́̉͆̊-̪͈͈̭͎͍̭̥̭͈̣̫̙͍̉̂́̋͆̉̾̄͋̇͌̈̏́̈̚.͓̗̰͙̖̥̠̭̱̙̯̃̀͗͆̃̈́͂͂͐͌͐ <117274957+Nkipohcs@users.noreply.github.com>
2022-11-30 15:01:37 +0100

    Add files via upload

---
+++ ft_printf/libft/ft_strnstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nschwob <nschwob@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 03:14:47 by nschwob           #+#    #+#             */
+/*   Updated: 2022/11/17 08:29:33 by nschwob          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	unsigned long	i;
+	size_t			j;
+
+	j = 0;
+	i = 0;
+	if (!haystack && !len)
+		return (0);
+	if (!*needle)
+		return ((char *)haystack);
+	while ((haystack[i]) && (size_t)i < len)
+	{
+		j = 0;
+		while (haystack[i] == needle[j] && haystack[i] && i < len)
+		{
+			i++;
+			j++;
+		}
+		if (!needle[j])
+			return ((char *)&haystack[i - j]);
+		i = (i - j) + 1;
+	}
+	return (NULL);
+}
